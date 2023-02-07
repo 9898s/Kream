6683da2a9a2ef777b24d57379f481f8ad8fcce36
9898s <sueh9898@icloud.com>
2023-02-07 12:42:34 +0900

    탑 버튼 애니메이션 추가

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(() => {
     // 스크롤 시 #top_btn 나타내기
     $(window).scroll(function () {
         let headerHeight = $("header").height();
-        $(this).scrollTop() >= headerHeight ? $("#top_btn").show() : $("#top_btn").hide();
+        $(this).scrollTop() >= headerHeight ? $("#top_btn").show(1000) : $("#top_btn").hide(1000);
     });
 
     // 배너 랜덤 이미지
